6c7d196271958c5acc02155e28268ac0ba9746c4
Bryan Alejandro <brayan186930@outlook.com>
2024-09-19 16:56:28 -0600

    Nueva tarea 2

--- Programas/data/norm_diab.py
+++ Programas/data/norm_diab.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 # Lee el archivo TXT
 df = pd.read_csv(r'C:\Users\bryan\Documents\ITQ\Semestre 8\Ciencia de Datos\DS env\Programas\data\diabetes.tab.txt', sep='\t')
@@ -7,10 +6,8 @@ df = pd.read_csv(r'C:\Users\bryan\Documents\ITQ\Semestre 8\Ciencia de Datos\DS e
 # Convierte a tipo float
 df = df.astype(float)
 
-# Escala los valores
-scaler = StandardScaler()
-df_scaled = scaler.fit_transform(df)
-df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
+# Normalización manual
+df_scaled = (df - df.mean()) / df.std()
 
 # Guarda los datos normalizados
 df_scaled.to_csv('diabetesnorm.csv', index=False)
