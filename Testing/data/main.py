06379b8c72a34d99d1c13d640011b16d8f33b24b
Bryan Alejandro <brayan186930@outlook.com>
2024-09-24 07:36:30 -0600

    Output Dir 1

--- Testing/data/main.py
+++ Testing/data/main.py
@@ -1,14 +1,5 @@
-# Regresion Lineal
-# 1) Hacer un split data con los datos normalizados de diabetesnorm.csv
-# - El 70% de columnas (6) tienen que ser aleatorias asi como el otro 30% (5 Columnas)
-# - Dataframe llamado test y train, dividir entre 70 y 30, el 30 es con la ultima columna.
-# 2) Generar el modelo de regresion lineal: linear_regression, con train_data y train_labels
-# - Columnas: 1-10 con train_data, 11 para train_labels
-# 3) Hacer la evaluacion como: y_pred = model.predict(test)
-# - Calcular el RMSE y MSE: MSE(y, y_pred), RMSE(y, y_pred)
-# 4) Hacer una funcion para cada uno de los pasos.
-# 5) Hacer linear_regression con train_data (1-10 columnas), train_labels (Columna 11)
-
+# Ciencia de Datos
+# Modelo entrenamiento y testing
 from imports import load_and_split_data, train_linear_regression, evaluate_model, plot_results
 
 # Ruta al archivo CSV
